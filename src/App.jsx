063e7399eba0389932350c9002f5e067ac6807d7
charzlwebz <charzlwebz256@gmail.com>
2023-04-20 17:06:51 +0300

    Delete ti

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ function App() {
 
   const addNewTodo = ()=>{
     setMyTodoList((prev)=>[...prev, newTodo]);
-  alert(newTodo);
+  // alert(newTodo);
     setMyTodo("");
+  };
+  const deleteTodo=(todo)=>{
+    setMyTodoList((prev)=>prev.filter((theTodo)=>theTodo !== todo));
   };
 
   return (
@@ -16,11 +19,36 @@ function App() {
       
          <h1>To do</h1>
          <p>You have {myTodoList.length} things to do</p>
-
+    <ol>
           {myTodoList.map((todo) =>(
 
-            <p>{todo}</p>
+            <li
+            style={{
+              display:"flex",
+              justifyContent: "space-between",
+              alignItems:"center",
+              fontFamily:'Times New Roman',
+              fontSize:'20'
+            }}
+            >
+              <button
+              onClick={()=>deleteTodo(todo)}
+              style={{
+                fontSize: 20,
+                margin: 15,
+                padding:4,
+                borderRadius: 10,
+                color:'black',
+                backgroundColor: 'white',
+                borderColor: 'Green',
+                fontFamily:'Times New Roman',
+              }}
+              >
+                Delete
+              </button>
+              {todo}</li>
           ))}
+          </ol>
           <input 
           type='text'
           value={newTodo}
